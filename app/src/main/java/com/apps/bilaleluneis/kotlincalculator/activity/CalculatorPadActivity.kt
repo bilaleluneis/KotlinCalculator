4fb46822f0c8191512b979b0e17072cbca5b3d1c
bilaleluneis <bilaleluneis@gmail.com>
2017-12-13 19:39:17 -0500

    added lazy init for buttons and used lambdas to filter through UI elements and get the buttons only , then set event handler for each programmatically.

--- app/src/main/java/com/apps/bilaleluneis/kotlincalculator/activity/CalculatorPadActivity.kt
+++ app/src/main/java/com/apps/bilaleluneis/kotlincalculator/activity/CalculatorPadActivity.kt
@@ -2,6 +2,9 @@ package com.apps.bilaleluneis.kotlincalculator.activity
 
 import android.app.Activity
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
+import android.util.Log
+import android.widget.Button
 import com.apps.bilaleluneis.kotlincalculator.R
 
 /**
@@ -12,8 +15,35 @@ import com.apps.bilaleluneis.kotlincalculator.R
 
 class CalculatorPadActivity : Activity() {
 
+    private val loggerTag = "CalculatorPadActivity"
+    private val calculatorButtons by lazy{ initCalculatorButtons() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-       setContentView(R.layout.activity_calculator_pad)
+        setContentView(R.layout.activity_calculator_pad)
+        calculatorButtons.forEach { it.setOnClickListener { onDigitClicked(it as Button) } }
+
     }
+
+    private fun initCalculatorButtons() : Array<Button>{
+
+        Log.d(loggerTag, "InitCalculatorButtons()")
+        val layout = findViewById<ConstraintLayout>(R.id.constrain_layout)
+        var buttons : Array<Button> = emptyArray()
+
+        (0 until layout.childCount)
+                .filter { index -> layout.getChildAt(index) is Button }
+                .map { index ->  buttons += layout.getChildAt(index) as Button}
+
+       return buttons
+
+    }
+
+    private fun onDigitClicked(button: Button){
+
+        Log.d(loggerTag,"button ${button.text} clicked !")
+
+    }
+
 }
