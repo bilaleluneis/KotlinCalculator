6f6b56d6bcde107d551b330e86c69603b49349d0
bilaleluneis <bilaleluneis@gmail.com>
2018-01-17 05:52:56 -0500

    minor update and cleanup for instrumented Test

--- app/src/androidTest/java/com/apps/bilaleluneis/kotlincalculator/CalculatorInstrumentedTest.kt
+++ app/src/androidTest/java/com/apps/bilaleluneis/kotlincalculator/CalculatorInstrumentedTest.kt
@@ -5,6 +5,7 @@ import android.support.test.espresso.action.ViewActions.click
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.espresso.matcher.ViewMatchers.withText
+import android.support.test.filters.SmallTest
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import com.apps.bilaleluneis.kotlincalculator.activity.CalculatorPadActivity
@@ -17,18 +18,18 @@ import org.junit.runner.RunWith
  * @author Bilal El Uneis
  * @since Jan 2018
  * bilaleluneis@gmail.com
+ * Instrumented test, which will execute on an Android device or Emulator.
  */
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
+@SmallTest
 class CalculatorInstrumentedTest {
 
-    @Rule @JvmField
-    val calculatorPadActivityTestRule =  ActivityTestRule(CalculatorPadActivity::class.java)
+    private val activity = CalculatorPadActivity::class.java
+
+    @Rule
+    @JvmField
+    val calculatorPadActivityTestRule =  ActivityTestRule(activity)
 
     @Test
     fun validateDisplayInitialState() {
